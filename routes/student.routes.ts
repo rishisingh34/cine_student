9eaf24d18a7cab986c157e86136159fb34300986
Prajal190904 <pranjalvyas45@gmail.com>
2024-06-23 21:30:19 +0530

    feat: added question response for test

--- routes/student.routes.ts
+++ routes/student.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 const router = Router();
 import authController from '../controllers/auth.controller'
+import testController  from '../controllers/test.controller'
+import auth from '../middleware/auth.middleware'
 
 router.post('/login',authController.login);
+router.post('/response',auth,testController.response);
 
 export default router;
