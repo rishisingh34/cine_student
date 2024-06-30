654b6ac46ced9b3700d128a9157e2aa098b277d6
rishisingh34 <singh34rishi@gmail.com>
2024-06-30 17:55:50 +0530

    chore: Update question model schema to include option sub-schema

--- models/question.model.ts
+++ models/question.model.ts
@@ -6,29 +6,26 @@ interface IOption {
 }
 
 interface IQuestion extends Document {
-  quesId: number;
+  quesId: number ;
   subject: string;
   question: string;
   options: IOption[];
   answer: number;
 }
 
+const optionSchema: Schema<IOption> = new Schema({
+  desc: { type: String, required: true },
+  id: { type: Number, required: true }
+}, { _id: false }); 
+
 const questionSchema: Schema<IQuestion> = new Schema({
   quesId: { type: Number, required: true },
   subject: { type: String, required: true },
-  question: { type: String, required: true },   
-  options: [{
-      desc: { type: String, required: true },
-      id: { type: Number, required: true }
-    }],
+  question: { type: String, required: true },
+  options: [optionSchema], 
   answer: { type: Number, required: true }
-});
+}, { versionKey: false });
 
 const Question: Model<IQuestion> = mongoose.model<IQuestion>('Question', questionSchema);
 
 export default Question;
-
-
-// question id { 1 - 50 or 60 } denotes the exact question and its category no need for category 
-// options.id { 1 - 4 } denotes the option number
-// answer { 1 - 4 } denotes the correct option number
